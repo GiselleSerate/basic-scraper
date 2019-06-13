ac752a28953d695c32c0928411cdbc13a4fe9272
GiselleSerate <giselle.serate@gmail.com>
2019-06-13 12:46:00 -0700

    Get token and update json from website

--- scraper.py
+++ scraper.py
@@ -1,12 +1,19 @@
+import json
 import os
+import re
 from time import sleep
 
 from bs4 import BeautifulSoup
 from flask import Flask
 from guerrillamail import GuerrillaMailSession
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 app = Flask(__name__)
@@ -43,7 +50,7 @@ class Scraper(object):
 
         # Set up driver
         chrome_options = Options()
-        chrome_options.add_argument('--headless')
+        # chrome_options.add_argument('--headless') # TODO for debugging purposes
         chrome_options.binary_location = binary_location
         self.driver = webdriver.Chrome(executable_path=os.path.abspath('chromedriver'), options=chrome_options)
 
@@ -64,10 +71,14 @@ class Scraper(object):
         # Wait for email to arrive
         while True:
             print('Making request.')
-            latest_summary = session.get_email_list()[0]
-            if(latest_summary.sender == self.email): # TODO there's a problem if we keep sending OTP emails from me. handle it
-                break
-            sleep(10)
+            try:
+                latest_summary = session.get_email_list()[0]
+                if(latest_summary.sender == self.email): # TODO there's a problem if we keep sending OTP emails from me. handle it
+                    break
+            except IndexError:
+                # Not enough mail, sleep anyway
+                pass
+            sleep(30)
         print('You\'ve got mail!')
         # Read the email with the OTP
         email = session.get_email(latest_summary.guid)
@@ -87,6 +98,8 @@ class Scraper(object):
     def login(self):
         # Load and identify login form
         self.driver.get(self.login_url)
+        sleep(10)
+        print(self.driver.page_source)
         emailBox = self.driver.find_element_by_id('Email') # TODO maybe check this lol idk
         pwdBox = self.driver.find_element_by_id('Password')
         submit = self.driver.find_element_by_class_name('loginbtn')
@@ -98,49 +111,48 @@ class Scraper(object):
         pwdBox.send_keys(self.password)
         submit.click()
 
-        print(self.driver.page_source) # TODO for now; later on verify this is okay and actually logged in
-
         # Get 2FA code from email
         otp = self.getOTP()
 
         # Submit the 2FA code
         otpBox = self.driver.find_element_by_id('otp')
-        submitOtp = self.driver.find_element_by_xpath('//*[@id="otp-form"]/div[2]/input')
+        submitOtp = self.driver.find_element_by_css_selector('#otp-form > div > input')
+        otpBox.click()
         otpBox.clear()
         otpBox.send_keys(otp)
-        submitOtp.click()
 
-        self.driver.get(self.update_url)
-        print(self.driver.page_source) # TODO verify that the page is interesting html (currently it's not.)
+        # Hover first so it lets you submit
+        hover = ActionChains(self.driver).move_to_element(submitOtp)
+        hover.perform()
+        otpBox.submit()
+
+        # Wait for page to load
+        timeout = 500
+        try:
+            dynamicHeader = EC.presence_of_element_located((By.ID, 'dynamicUpdates'))
+            WebDriverWait(self.driver, timeout).until(dynamicHeader)
+        except TimeoutException:
+            print('Timed out waiting for post-login page to load.')
+
+        # Get Request Verification Token
+        token = self.driver.find_element_by_name('__RequestVerificationToken')
+        match = re.search(r'"data":({"Data":.*?"Total":\d+,"AggregateResults":null})', self.driver.page_source)
+        if match is None:
+            raise GetLinkError("You have no access to download files. Probably some hardcoded URL is wrong, or you set wrong 'companyid' in config file.")
+        updates = json.loads(match.group(1))
+        return token, updates
+
+    # def get_update_page(self):
+    #     '''
+    #     Must start on update page. 
+    #     '''
+
+
 
 
 if __name__ == '__main__':
     scraper = Scraper(email=app.config['EMAIL'], password=app.config['PASSWORD'], company_id=app.config['COMPANY_ID'],\
         package="appthreat", debug=False, isReleaseNotes=False, binary_location=app.config['BINARY_LOCATION'])
-    scraper.login()
-
-
-# self.browser.form['Email'] = self.username
-# self.browser.form['Password'] = self.password
-# self.last_request = datetime.now()
-# self.browser.submit()
-
-# about = driver.find_element_by_xpath('/html/body/ul/li[2]/a')
-# linkTo = about.get_attribute('href')
-
-# htmlSrc = driver.get(linkTo)
-
-# print(driver.page_source)
-
-# print(about.get_attribute('href'))
-# print(about.get_attribute('title'))
-# if about.is_displayed():
-#   about.click()
-#   print('here')
-
-
-# search_field = driver.find_element_by_id('site-search')
-# search_field.clear()
-# search_field.send_keys('Olabode')
-# search_field.send_keys(Keys.RETURN)
-# assert 'Looking Back at Android Security in 2016' in driver.page_source   driver.close()
+    token, updates = scraper.login()
+    print(updates)
+    # scraper.nav_to_updates()
